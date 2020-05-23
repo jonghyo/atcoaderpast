f77c68037764be1ec3ec7aa95d64f2f40f533c4b
jonghyo <m.73816686@gmail.com>
2020-05-23 23:03:40 +0900

    change README.md.

--- src/Tt.kt
+++
@@ -1,9 +0,0 @@
-fun main(args: Array<String>) {
-    torT()
-}
-
-inline fun torT(){
-    val list = readLine()!!.split(" ").map(String::toInt)
-    val max = if (list[0] * list[1] < list[2]) list[0] * list[1] else list[2]
-    println(max)
-}
